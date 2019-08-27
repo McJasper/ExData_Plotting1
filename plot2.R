22aeffdfc0668e63bacbd4ef452e7b84c71f3468
McJasper <mcjustinosu@gmail.com>
2019-08-27 16:17:00 +0200

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,26 @@
+# Moved the file caching code out to keep this script lean.
+# In short, this file reads in the zip data, unzips, searches for the needed
+# rows in the Date column, converts to the correct class, determines which rows 
+# in the zipped table to read, and reads only those in.
+# Finally, the data column of the required rows is formatted to the correct class
+# and the environment is cleaned.
+
+setwd(Your_wd_here)
+source("sourceData.R")
+
+# Plot1
+png(filename = "plot1.png", width = 480, height = 480)
+plot(x = rawData$Time, y = rawData$Global_active_power,
+     ylab = "Global Active Power (kilowatts)"
+)
+axis(side = 1)
+substr(wday(rawData$Date, label = T, abbr = T), start = 1, stop = 3)
+dev.off()
+
+
+
+
+
+
+
+
