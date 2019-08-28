fe7960609f517a3565376431ebec071714ae953c
McJasper <mcjustinosu@gmail.com>
2019-08-28 11:37:46 +0200

    Final edits

--- plot3.R
+++ plot3.R
@@ -1,5 +1,5 @@
 # Moved the file caching code out to keep this script lean.
-# In short, this file reads in the zip data, unzips, searches for the needed
+# In short sourceData.R: reads in the zip data, unzips, searches for the needed
 # rows in the Date column, converts to the correct class, determines which rows 
 # in the zipped table to read, and reads only those in.
 # Finally, the data column of the required rows is formatted to the correct class
@@ -9,10 +9,15 @@ setwd(Your_wd_here)
 source("sourceData.R")
 
 # Plot3
-png(filename = "plot1.png", width = 480, height = 480)
-hist(x = rawData$Global_active_power, col = "red",
-     main = "Global Active Power", xlab = "Global Active Power (kilowatts",
-     ylab = "Frequency")
+png(filename = "plot3.png", width = 480, height = 480)
+
+plot(x=rawData$DateTime, y = rawData$Sub_metering_1, type = "l",
+     xlab = "", ylab = "Energy sub metering", col = "black")
+lines(x=rawData$DateTime, y = rawData$Sub_metering_2, col ="red")
+lines(x=rawData$DateTime, y = rawData$Sub_metering_3, col ="blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       col = c("black", "red", "blue"), lty = 1)
+
 dev.off()
 
 
