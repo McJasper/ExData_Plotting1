7cf1b78c6a75a3cd8e7153ba8e71b5fb694dec8a
McJasper <mcjustinosu@gmail.com>
2019-08-28 11:38:04 +0200

    Final edits

--- plot4.R
+++ plot4.R
@@ -9,10 +9,29 @@ setwd(Your_wd_here)
 source("sourceData.R")
 
 # Plot4
-png(filename = "plot1.png", width = 480, height = 480)
-hist(x = rawData$Global_active_power, col = "red",
-     main = "Global Active Power", xlab = "Global Active Power (kilowatts",
-     ylab = "Frequency")
+
+png(filename = "plot4.png", width = 480, height = 480)
+
+par(mfrow=c(2,2))
+#SubPlot1
+plot(x = rawData$DateTime, y = rawData$Global_active_power,
+     ylab = "Global Active Power", type = "l", xlab = "")
+#SubPlot2
+plot(x = rawData$DateTime, y=rawData$Voltage, type = "l", 
+     xlab= "datetime", ylab = "Voltage")
+
+#SubPlot3
+plot(x=rawData$DateTime, y = rawData$Sub_metering_1, type = "l",
+     xlab = "", ylab = "Energy sub metering", col = "black")
+lines(x=rawData$DateTime, y = rawData$Sub_metering_2, col ="red")
+lines(x=rawData$DateTime, y = rawData$Sub_metering_3, col ="blue")
+legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       col = c("black", "red", "blue"), lty = 1, bty = "n")
+
+#SubPlot4
+plot(x = rawData$DateTime, y = rawData$Global_reactive_power, type = "l",
+     xlab = "datetime", ylab = "Global_reactive_power")
+
 dev.off()
 
 
